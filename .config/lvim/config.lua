9d4076423b33fab731cf4128b5937d03fb21757b
Vinit Neogi <20491952+vneogi199@users.noreply.github.com>
2023-08-14 00:22:55 +0530

    add lvim config

---
+++ .config/lvim/config.lua
@@ -0,0 +1,14 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+
+-- Install catppuccin color scheme
+lvim.plugins = {
+  { "catppuccin/nvim" }
+}
+
+
+-- Set lvim settings:
+lvim.colorscheme="catppuccin-frappe"
