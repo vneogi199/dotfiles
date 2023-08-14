d4d0c03c52a0f8093fddf9f92e23dd63f8ec64d2
Vinit Neogi <20491952+vneogi199@users.noreply.github.com>
2023-08-14 14:13:53 +0530

    update config.lua

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -13,10 +13,10 @@ lvim.plugins = {
 vim.opt.number = true
 vim.opt.relativenumber = true
 
-
 -- Set lvim settings
 lvim.colorscheme = "catppuccin-frappe"
 lvim.builtin.lualine.style = "default"
+lvim.builtin.terminal.open_mapping = "<C-l>"
 lvim.format_on_save.enabled = true
 
 -- Set lvim keyboard mappings
@@ -24,3 +24,6 @@ lvim.keys.normal_mode["<C-d>"] = "<C-d>zz" -- center screen on page down
 lvim.keys.normal_mode["<C-d>"] = "<C-d>zz" -- center screen on page down
 lvim.keys.normal_mode["<C-f>"] = "<C-f>zz" -- center screen on page up half screen
 lvim.keys.normal_mode["<C-b>"] = "<C-b>zz" -- center screen on page up half screen
+lvim.keys.normal_mode["n"] = "nzz"         -- center screen on next result
+lvim.keys.normal_mode["N"] = "Nzz"         -- center screen on previous result
+lvim.keys.insert_mode["kj"] = "<Esc>"      -- go to normal mode on pressing kj
