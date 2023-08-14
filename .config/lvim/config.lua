5644e1239a70e7fdcd75c99f52ed677a5dd54d9d
Vinit Neogi <20491952+vneogi199@users.noreply.github.com>
2023-08-14 13:35:00 +0530

    update config.lua

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -10,15 +10,17 @@ lvim.plugins = {
 }
 
 -- Set vim settings
-vim.opt.number=true
-vim.opt.relativenumber=true
+vim.opt.number = true
+vim.opt.relativenumber = true
 
 
 -- Set lvim settings
-lvim.colorscheme="catppuccin-frappe"
+lvim.colorscheme = "catppuccin-frappe"
+lvim.builtin.lualine.style = "default"
+lvim.format_on_save.enabled = true
 
 -- Set lvim keyboard mappings
-lvim.keys.normal_mode["<C-d>"]="<C-d>zz" -- center screen on page down
-lvim.keys.normal_mode["<C-d>"]="<C-d>zz" -- center screen on page down
-lvim.keys.normal_mode["<C-f>"]="<C-f>zz" -- center screen on page up half screen
-lvim.keys.normal_mode["<C-b>"]="<C-b>zz" -- center screen on page up half screen
+lvim.keys.normal_mode["<C-d>"] = "<C-d>zz" -- center screen on page down
+lvim.keys.normal_mode["<C-d>"] = "<C-d>zz" -- center screen on page down
+lvim.keys.normal_mode["<C-f>"] = "<C-f>zz" -- center screen on page up half screen
+lvim.keys.normal_mode["<C-b>"] = "<C-b>zz" -- center screen on page up half screen
