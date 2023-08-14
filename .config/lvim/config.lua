536badc8b1e73c8741fbb1c987a3ba41df6c083d
Vinit Neogi <20491952+vneogi199@users.noreply.github.com>
2023-08-14 13:10:38 +0530

    add lvim config for Ctrl+f and Ctrl+b

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -9,6 +9,16 @@ lvim.plugins = {
   { "catppuccin/nvim" }
 }
 
+-- Set vim settings
+vim.opt.number=true
+vim.opt.relativenumber=true
 
--- Set lvim settings:
+
+-- Set lvim settings
 lvim.colorscheme="catppuccin-frappe"
+
+-- Set lvim keyboard mappings
+lvim.keys.normal_mode["<C-d>"]="<C-d>zz" -- center screen on page down
+lvim.keys.normal_mode["<C-d>"]="<C-d>zz" -- center screen on page down
+lvim.keys.normal_mode["<C-f>"]="<C-f>zz" -- center screen on page up half screen
+lvim.keys.normal_mode["<C-b>"]="<C-b>zz" -- center screen on page up half screen
